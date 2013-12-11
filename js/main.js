519b8ff7281394e523dcf105b838a8b728a39d7c
Cholo De Villa <cholodevilla@gmail.com>
2013-12-12 04:38:13 +0800

    New Blog Design Assets

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(function(){
+	headerToggleInit();
+	function headerToggleInit(){
+		var open = false;
+		$('#filter-btn').click(function(){
+			open = headerToggle(open);
+		});
+		$("#filter-options a").css( "width", 0 );
+	}
+	function headerToggle(state){
+		if(state === false) {
+			$("#filter-indicator").animate({width: 80}, 250);
+			$("#filter-options a").animate({width: 0}, 500);
+			return true;
+		} else {
+			$("#filter-indicator").animate({width: 0}, 250);
+			$("#filter-options a").animate({width: 80}, 500);
+			return false;
+		}
+	}
+});
+
